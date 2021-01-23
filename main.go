cbcac289b99d4bf7097571d774fc5b64c9b04c97
unknown <97paosba@gmail.com>
2021-01-23 16:27:06 -0500

    Codigo de suma completo

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
+var a, b int
+
 func main() {
 
+}
+func suma() {
+	println("Escriba los numeros a sumar")
+	print("Numero 1: ")
+	fmt.Scan(&a)
+	print("Numero 2: ")
+	fmt.Scan(&b)
+	println("La suma es: ", a+b)
 }
