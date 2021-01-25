6b20499cf8ba9477406290c551b7242361599df4
unknown <97paosba@gmail.com>
2021-01-25 11:46:13 -0500

    Operaciones con punto flotante

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
-var c, d float32
-var a, b int
+var a, b float32 //inicializa las variables en un flotante de 32bits
 
 func main() {
-
+	multiplicacion()
 }
 func opge() {
 	print("Numero 1: ")
@@ -18,25 +17,25 @@ func opge() {
 func suma() {
 	println("Digite los numeros a sumar")
 	opge()
-	println("La suma es: ", a+b)
+	fmt.Printf("La suma es: %.2f\n", a+b)
 	//volver()
 }
 func resta() {
 	println("Digite los numeros a restar")
 	opge()
-	println("La resta es: ", a-b)
+	fmt.Printf("La resta es: %.2f\n", a-b)
 	//volver()
 }
 func multiplicacion() {
 	println("Digite los numeros a multiplicar")
 	opge()
-	println("El resultado de la multiplicacón es: ", a*b)
+	fmt.Printf("El resultado de la multiplicacón es: %.2f\n", a*b)
 	//volver()
 }
 
 func division() {
 	println("Digite los numeros a dividir")
 	opge()
-	fmt.Printf("El cociente es: %f\n", c/d)
+	fmt.Printf("El cociente es: %.2f\n", a/b) //para limitar la cantidad de decimales usar .xf, donde x es el # de decimales
 	//volver()
 }
