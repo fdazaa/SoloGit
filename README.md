3fad65f7d1c2c43141fd6b31260f01f736379cf1
jforero <jhffr96@hotmail.com>
2021-01-25 11:46:54 -0500

    funcion de porcentaje

--- README.md
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-# SoloGit
-Pruebas Git
-Codigo complete que realiza la operación de division entre dos numeros
-=======
-## Calculadora 
-Este código corresponde a la un calculadora de suma, resta, multiplicacion, division
-
->>>>>>> develop
--- main.go
+++ main.go
@@ -2,72 +2,15 @@ package main
 
 import "fmt"
 
-var c, d float32
-var a, b int
-
 func main() {
-	var operacion int
-	fmt.Println("\nCALCULADORA GO")
-	fmt.Println("Digite el número del tipo de operacion a realizar")
-
-	fmt.Println("\n(1) SUMA")
-	fmt.Println("(2) RESTA")
-	fmt.Println("(3) MULTIPLICACION")
-	fmt.Println("(4) DIVISIÓN")
-
-	fmt.Println("__")
-	fmt.Print("> ")
-	fmt.Scan(&operacion)
-	fmt.Println("")
-
-	switch operacion {
-	case 1:
-		suma()
-	case 2:
-		resta()
-	case 3:
-		println("MULTIPLICACIÓN")
-	case 4:
-		division()
-	}
+	porcentaje()
 }
 
-func suma() {
-	println("Digite los numeros a sumar")
-	print("Numero 1: ")
+func porcentaje() {
+	var a, b float32
+	println("Digita un numero:")
 	fmt.Scan(&a)
-	print("Numero 2: ")
+	println("Que porcentaje quieres saber?:")
 	fmt.Scan(&b)
-	println("La suma es: ", a+b)
-	volver()
-}
-func resta() {
-	println("Digite los numeros a restar")
-	print("Numero 1: ")
-	fmt.Scan(&a)
-	print("Numero 2: ")
-	fmt.Scan(&b)
-	println("La resta es: ", a-b)
-	volver()
-}
-
-func division() {
-	println("Digite los numeros a dividir")
-	print("Numero 1: ")
-	fmt.Scan(&c)
-	print("Numero 2: ")
-	fmt.Scan(&d)
-	fmt.Printf("El cociente es: %f\n", c/d)
-	volver()
-}
-
-func volver() {
-	var volver int
-	fmt.Println("Presione 1 para volver")
-	fmt.Scan(&volver)
-	if volver == 1 {
-		main()
-	} else {
-		fmt.Println("Chao!")
-	}
+	fmt.Printf("resultado: %.2f\n", (a*b)/100)
 }
